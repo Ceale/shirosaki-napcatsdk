2073b3e62ab1cfaa8404e4f79346520814c0193d
Ceale <clxh100100@hotmail.com>
2025-11-13 03:39:05 +0800

    更改：暂时移除了NCClient内置的NCSelf实例。初步实现NCSelf的部分功能

--- src/service/NCSelf.ts
+++ src/service/NCSelf.ts
@@ -1,6 +1,7 @@
 import type { Logger } from "../util/Logger"
-import type { NapCatClient } from "../NapCatClient"
+import { NC_CLIENT_STATE, type NapCatClient } from "../NapCatClient"
 import { BindThis } from "../util/AutoBind"
+import { assert } from "@ceale/util"
 
 interface NCSelfProvider {
     NCClient: NapCatClient
@@ -19,6 +20,9 @@ export class NCSelf {
 
     private readonly debug: boolean
 
+    public readonly waitReady: Promise<void>
+    public isReady = false
+
     /**
      * @param provider 所需依赖，详见 {@link NCMessageProvider}
      * @param config 配置项，详见 {@link NCMessageConfig}
@@ -34,25 +38,93 @@ export class NCSelf {
         this.NCClient = NCClient
         this.Logger = Logger
         this.debug = debug
-
-        NCClient.onEvent("meta_event.lifecycle.connect", this.refreshSelfInfo)
+        
+        this.waitReady = new Promise(async resolve => {
+            if (this.NCClient.state === NC_CLIENT_STATE.OPEN) {
+                const { data } = await this.NCClient.sendAction("get_login_info")
+                this._user_id = data.user_id
+                await this.regetInfo()
+                this.isReady = true
+                resolve()
+            } else {
+                NCClient.onceEvent("meta_event.lifecycle.connect", async (data) => {
+                    this._user_id = data.self_id
+                    await this.regetInfo()
+                    this.isReady = true
+                    resolve()
+                })
+            }
+        })
     }
 
-    private _selfInfo?: {
+    private _raw_info?: any
+    public get raw_info() {
+        return this._raw_info
+    }
+
+    public async regetInfo() {
+        if (!this.user_id) return false
+        const data = await this.NCClient.sendAction("get_stranger_info", { user_id: this.user_id })
+        if (data?.retcode !== 0) return false
+        this._raw_info = data?.data
+        this._nickname = data?.data.nickname
+        
+    }
+
+    private _user_id?: string
+    public get user_id() {
+        return this._user_id
+    }
+
+    private _nickname?: string
+    public get nickname() {
+        return this._nickname
+    }
+    public async setNickname(name: string) {
+        this.NCClient.sendAction("get_login_info")
+    }
+
+    // private _a
+    public getAvatar() {
+        const size = 0 // 原图
+        const url = `https://thirdqq.qlogo.cn/g?b=sdk&s=${size}&nk=${user_id}`
+    }   
+
+    // 1
+
+    private _info?: {
         user_id: number,
         nickname: string,
     }
 
-    public get selfInfo() {
-        return this._selfInfo
+    public get info() {
+        return this._info
     }
 
-
     @BindThis
-    public async refreshSelfInfo() {
+    public async refreshInfo() {
         const data = await this.NCClient.sendAction("get_login_info")
         if (data?.retcode !== 0) return false
-        this._selfInfo = data?.data
+        this._info = data?.data
         return true
+    }
+
+    public async setInfo(/* ...code... */) {
+        // ...code...
+    }
+
+    // 2
+    
+    public info?: {
+        user_id: number,
+        nickname: string,
+    }
+
+    public async getInfo() {
+        return await this.NCClient.sendAction("get_login_info")
+    }
+
+    public async setInfo(/* ...code... */) {
+        // ...code...
     }
 }
