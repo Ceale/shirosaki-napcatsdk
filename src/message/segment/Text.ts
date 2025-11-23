fa01a2d0ee442cf674debcd39729150777441d11
Ceale <clxh100100@hotmail.com>
2025-11-23 11:28:18 +0800

    新增：新增了Text类

---
+++ src/message/segment/Text.ts
@@ -0,0 +1,22 @@
+import type { anyobject } from "@ceale/util"
+import { MessageSegment } from "../MessageSegment"
+
+export class Text extends MessageSegment {
+
+    constructor(public text: string) {
+        super()
+    }
+
+    toJSON() {
+        return {
+            type: "text",
+            data: {
+                text: this.text
+            }
+        }
+    }
+
+    static fromJSON(obj: anyobject) {
+        return new this(obj?.data?.text ?? "")
+    }
+}
