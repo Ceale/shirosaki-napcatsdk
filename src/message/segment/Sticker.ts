9c1c5886c5c77e4165b13fd628acfe500e9cc8c6
Ceale <clxh100100@hotmail.com>
2025-11-23 11:29:45 +0800

    新增：新增了 Sticker 类

---
+++ src/message/segment/Sticker.ts
@@ -0,0 +1,46 @@
+import { type anyobject, tryCatch } from "@ceale/util"
+import { MessageSegment } from "../MessageSegment"
+
+export class Sticker extends MessageSegment { 
+    
+    constructor(
+        public data: string | Buffer,
+        public summary?: string
+    ) {
+        super()
+    }
+    
+    static fromJSON(json: anyobject) {
+        return new this(
+            json?.data?.url ?? "",
+            json?.data?.summary ?? undefined
+        )
+    }
+
+    toJSON() {
+        const data = Buffer.isBuffer(this.data)
+            ? "data:application/octet-stream;base64," + this.data.toString("base64")
+            : this.data
+        return {
+            type: "image",
+            data: {
+                sub_type: 1,
+                file: data,
+                summary: this.summary ?? undefined,
+            }
+        }
+    }
+
+    async getData() {
+        if (Buffer.isBuffer(this.data)) {
+            return this.data
+        } else {
+            const [ data ] = await tryCatch((await fetch(this.data)).arrayBuffer())
+            if (data) {
+                return Buffer.from(data)
+            } else {
+                return Buffer.alloc(0)
+            }
+        }
+    }
+}
