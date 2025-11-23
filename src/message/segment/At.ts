3036a2ee3deab68f0ad9b349524a09b0a331a365
Ceale <clxh100100@hotmail.com>
2025-11-23 11:28:33 +0800

    新增：新增了At类

---
+++ src/message/segment/At.ts
@@ -0,0 +1,25 @@
+import type { anyobject } from "@ceale/util"
+import { MessageSegment } from "../MessageSegment"
+
+export class At extends MessageSegment {
+
+    public target: string
+
+    constructor(target: string&{} | number | "all") {
+        super()
+        this.target = String(target)
+    }
+
+    static fromJSON(obj: anyobject) {
+        return new this(obj?.data?.qq ?? 0)
+    }
+
+    toJSON() {
+        return {
+            type: "at",
+            data: {
+                qq: this.target
+            }
+        }
+    }
+}
