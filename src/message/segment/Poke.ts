7eba2bca03a1ba52427c9e4adcbb641ce981017b
Ceale <clxh100100@hotmail.com>
2025-11-23 11:29:18 +0800

    新增：新增了 Poke 类

---
+++ src/message/segment/Poke.ts
@@ -0,0 +1,29 @@
+import type { anyobject } from "@ceale/util"
+import { MessageSegment } from "../MessageSegment"
+
+export class Poke extends MessageSegment {
+
+    constructor(
+        public type: string,
+        public id: string
+    ) {
+        super()
+    }
+
+    toJSON() {
+        return {
+            type: "poke",
+            data: {
+                type: this.type,
+                id: this.id
+            }
+        }
+    }
+
+    static fromJSON(obj: anyobject) {
+        return new this(
+            obj?.data?.type ?? "",
+            obj?.data?.id ?? ""
+        )
+    }
+}
