9793418fce5199e08b61f91707513eef64f0c430
Ceale <clxh100100@hotmail.com>
2025-11-23 11:29:25 +0800

    新增：新增了 Reply 类

---
+++ src/message/segment/Reply.ts
@@ -0,0 +1,22 @@
+import type { anyobject } from "@ceale/util"
+import { MessageSegment } from "../MessageSegment"
+
+export class Reply extends MessageSegment {
+
+    constructor(public id: string) {
+        super()
+    }
+
+    toJSON() {
+        return {
+            type: "reply",
+            data: {
+                id: this.id
+            }
+        }
+    }
+
+    static fromJSON(obj: anyobject) {
+        return new this(obj?.data?.id ?? "0")
+    }
+}
